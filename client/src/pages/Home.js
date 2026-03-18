ce3a50aa5ca09772b382e2c45656797528d8c7b7
aryan-maurya-07 <aryanmaurya650@gmail.com>
2026-03-19 01:27:11 +0530

    fix warning

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaRecycle, FaGift, FaLeaf } from "react-icons/fa";
+
 
 function Home() {
   return (
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,7 +7,7 @@ function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+
 
   const handleLogin = async (e) => {
 
